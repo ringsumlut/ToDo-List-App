3a7ce52676dd385f4ad35d897a464981efc7010a
Ring Sumlut <ringsumlut@gmail.com>
2021-09-29 00:22:36 -0400

    delete updated

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,26 +1,42 @@
 import React from 'react';
 
-import {List, Grid, Label, Button} from 'semantic-ui-react';
+import { List, Grid, Label, Button } from 'semantic-ui-react';
 
-const Task = ({name, color})=> {
-    console.log(name, color);
-    return(
-        <React.Fragment>
-             <List.Item>
-              <Grid columns='2'>
-                <Grid.Column>
-                  <Label color={color} size='big'>
-                    {name}
-                  </Label>
-                  </Grid.Column>
-                <Grid.Column textAlign='right'>
-                  <Button icon='trash' color='red'></Button>
-                  <Button icon='pencil' color='orange'></Button>
-                </Grid.Column>
-              </Grid>
-            </List.Item>
-        </React.Fragment>
-    );
+const Task = ({ name, color, editTask, deleteTask, index, }) => {
+
+  function editCurrentTask() {
+    editTask(index);
+  }
+  function deleteCurrentTask() {
+    deleteTask();
+  }
+
+  return (
+    <React.Fragment>
+      <List.Item>
+        <Grid columns='2'>
+          <Grid.Column>
+            <Label color={color} size='big'>
+              {name}
+            </Label>
+          </Grid.Column>
+          <Grid.Column textAlign='right'>
+            <Button
+              icon='trash'
+              onClick={deleteCurrentTask}
+              color='red'
+            >
+            </Button>
+            <Button
+              icon='pencil'
+              onClick={editCurrentTask}
+              color='orange'>
+            </Button>
+          </Grid.Column>
+        </Grid>
+      </List.Item>
+    </React.Fragment>
+  );
 };
 
 export default Task;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,112 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.css';
-import { Header, Container, Button, Grid, Segment, Form, Select, List } from 'semantic-ui-react';
+import { Header, Container, Button, Grid, List } from 'semantic-ui-react';
 import Task from '../components/Task';
+import NewTaskForm from '../components/NewTaskForm';
 
 const HomePage = () => {
+
+  const initialNewTask = {
+    name: '',
+    color: '',
+  }
+  const [newTaskOpen, setNewTaskOpen] = React.useState(false);
+  const [newTask, setNewTask] = React.useState(initialNewTask);
+  const [list, setList] = React.useState([]);
+  //const [lists, setLists] = React.useState("");
+
+  function openNewTask() {
+    setNewTaskOpen(true);
+  }
+  function closeNewTask() {
+    setNewTaskOpen(false);
+  }
+  function showNewTaskOpen() {
+    //console.log(newTaskOpen);
+  }
+
+  function addNewTask() {
+    const listClone = [...list];
+    listClone.push(newTask);
+    setList(listClone);
+    setNewTask(initialNewTask);
+    closeNewTask();
+  }
+
+
+  // const taskList =[];
+
+  // list.forEach((task, index) => {
+  // taskList.push(
+  //   <Task 
+  //     key={`${task.name}-${index}`} 
+  //     name={task.name} 
+  //     color={task.color} 
+  //   />
+  // );
+  // });
+
+  function editTask(index) {
+    console.log("edit", index);
+    const newList = list.map((task, i) => {
+      if (i !== index) return task;
+      return {
+        name: `Edit ${task.name}`,
+        color: task.color,
+      }
+
+    });
+    setList(newList);
+  }
+  function deleteTask(index) {
+    console.log("delete", index);
+    const updateTask = [...list].filter((list) => list.index !== index);
+
+    setList(updateTask);
+  }
+  const taskList = list.map((task, index) => {
+    return (
+      <Task
+        key={`${task.name}-${index}`}
+        name={task.name}
+        color={task.color}
+        editTask={editTask}
+        deleteTask={deleteTask}
+        index={index}
+
+
+      />
+    );
+
+  });
+
+
   return (
     <React.Fragment>
       <Container>
         <Grid>
           <Grid.Column width='4'>
-            <Button icon='bars'></Button>
+            <Button icon='bars' onClick={showNewTaskOpen}></Button>
           </Grid.Column>
-        
-        <Grid.Column width='8'>
+
+          <Grid.Column width='8'>
             <Header textAlign='center' as='h1'>Todo List</Header>
-        </Grid.Column>
-          
+          </Grid.Column>
+
           <Grid.Column textAlign='right' width='4'>
-            <Button color='green' icon='plus'></Button>
+            <Button color='green' icon='plus' onClick={openNewTask}></Button>
           </Grid.Column>
         </Grid>
-
-        <Segment>
-          <Header as='h2'>New Task</Header>
-          <Form>
-            <Form.Field
-              control='input'
-              label="Task Name"
-              placeholder='Enter task name'
-            />
-            <Form.Field
-              control={Select}
-              label='Task Color'
-              placeholder='Choose task color...'
-              options={[
-                { text: 'Red', value: 'red'},
-                { text: 'Yellow', value: 'yellow'},
-                { text: 'Green', value: 'green'}
-              ]}
-              />
-              <Button.Group fluid>
-                <Button  color='red'>Cancel</Button>
-                <Button.Or />
-                <Button  color='green'>Add Task</Button>
-              </Button.Group>
-
-          </Form>
-        </Segment>
+        {newTaskOpen ? (<NewTaskForm
+          closeNewTask={closeNewTask}
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addNewTask={addNewTask}
+        />
+        ) : null}
         <List>
-            <Task name='Task 1' color='yellow'></Task>
-            <Task name='Task 2' color='red'></Task>
-            <Task name='Task 3' color='green'></Task>
+          {taskList}
+
         </List>
       </Container>
     </React.Fragment>
